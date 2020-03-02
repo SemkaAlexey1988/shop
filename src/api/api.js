4bbd29f0f34601ec9bdfc44be9bea7a52625a331
alexey <vorzelski@gmail.com>
2020-03-02 22:01:53 +0200

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 export default class Api {
 
-_shopUrl = 'https://test.herokuapp.com';
-_applicationUrl = 'http://test.com';
+_shopUrl = 'https://test2.herokuapp.com';
+_applicationUrl = 'http://test2.com';
 
 }
 
